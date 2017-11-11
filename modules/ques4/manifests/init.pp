d786ffec2dd0a8b4ea4c1ae4081710d466096ba7
root <root@ip-172-31-16-151.us-west-2.compute.internal>
2017-11-11 15:30:47 +0000

    added sudoer user using augeas

--- modules/ques4/manifests/init.pp
+++ modules/ques4/manifests/init.pp
@@ -52,4 +52,25 @@ augeas { "disablerootforssh":
 	notify  =>  Service['sshd']  
 }  
 
+# change sudoers to allow becca to sudo to a root shell
+#
+
+file { "/etc/sudoers":
+      owner   => "root",
+      group   => "root",
+      mode    => "440",
+     }
+
+ 
+augeas { "sudobecca":
+	context => "/files/etc/sudoers",
+	changes => [
+	"set spec[user = 'becca']/user becca",
+	"set spec[user = 'becca']/host_group/host ALL",
+	"set spec[user = 'becca']/host_group/command ALL",
+	"set spec[user = 'becca']/host_group/command/runas_user ALL",
+	 ],
+}
+
+
 }
