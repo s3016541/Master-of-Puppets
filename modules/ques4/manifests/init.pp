4d8326824473291969c1772c549094423fcdd28d
root <root@ip-172-31-16-151.us-west-2.compute.internal>
2017-11-11 14:11:51 +0000

    Question 4 - config managment

---
+++ modules/ques4/manifests/init.pp
@@ -0,0 +1,55 @@
+# Class: ques4
+# ===========================
+#
+# Full description of class ques4 here.
+#
+# Parameters
+# ----------
+#
+# Document parameters here.
+#
+# * `sample parameter`
+# Explanation of what this parameter affects and what it defaults to.
+# e.g. "Specify one or more upstream ntp servers as an array."
+#
+# Variables
+# ----------
+#
+# Here you should define a list of variables that this module would require.
+#
+# * `sample variable`
+#  Explanation of how this variable affects the function of this class and if
+#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#  External Node Classifier as a comma separated list of hostnames." (Note,
+#  global variables should be avoided in favor of class parameters as
+#  of Puppet 2.6.)
+#
+# Examples
+# --------
+#
+# @example
+#    class { 'ques4':
+#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    }
+#
+# Authors
+# -------
+#
+# Author Name <author@domain.com>
+#
+# Copyright
+# ---------
+#
+# Copyright 2017 Your name here, unless otherwise noted.
+#
+class ques4 {
+
+# disable root login for ssh
+
+augeas { "disablerootforssh":  
+        context => "/files/etc/ssh/sshd_config",  
+        changes => "set PermitRootLogin no",  
+	notify  =>  Service['sshd']  
+}  
+
+}
